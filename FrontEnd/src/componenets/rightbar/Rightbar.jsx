54bd2f983b63df2db1a65d48f52957a85b783785
magfurrume <40976169+magfurrume@users.noreply.github.com>
2022-12-19 00:48:33 +0600

    Rightbar Done

--- FrontEnd/src/componenets/rightbar/Rightbar.jsx
+++ FrontEnd/src/componenets/rightbar/Rightbar.jsx
@@ -3,6 +3,98 @@ import "./rightbar.css";
 
 export default function Rightbar() {
   return (
-    <div className='ritghtbar'>Rightbar</div>
+    <div className='ritghtbar'>
+      <div className="rightbarWrapepr">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText"><b>Ishmam</b> and <b>2 other friends</b> have birthday today</span>
+        </div>
+        <img className='rightbarAd' src="/assets/ad.jpg" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/ishmam.jpg" alt="" className="rightProfileImg" />
+              <span className="rightbarOnline">
+
+              </span>
+            </div>
+            <span className="rightbarUsername">Ishmam</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
