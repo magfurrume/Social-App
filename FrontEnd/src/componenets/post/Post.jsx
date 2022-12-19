df44b58dcae5087cc787bacf4b733b9d7d14420f
magfurrume <40976169+magfurrume@users.noreply.github.com>
2022-12-19 22:12:10 +0600

    feedUpdated v1

--- FrontEnd/src/componenets/post/Post.jsx
+++ FrontEnd/src/componenets/post/Post.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import "./post.css";
 import { MoreVert } from '@material-ui/icons';
+import { Users } from "../../dummyData"
 
-export default function Post() {
+export default function Post({ post }) {
+ const findUser = Users.filter((u) => u.id === post.userId); // Find user Along with post
     return (
         <div className='post'>
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="/assets/person/pirsaheb.jpg" alt="" className="postProfileImg" />
-                        <span className="postUsername">Pir Saheb</span>
-                        <span className="postDate">5 mins ago</span>
+                        <img src={findUser[0].profilePicture} alt="" className="postProfileImg" />
+                        <span className="postUsername">{findUser[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">My First Text</span>
-                    <img src="/assets/post/1.jpg" alt="" className="postImg" />
+                    <span className="postText">{post?.desc}</span>
+                    <img src={post.photo} alt="" className="postImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className="postLike" src="/assets/like.png" alt="" />
                         <img className="postLike" src="/assets/heart.png" alt="" />
-                        <span className="postLikeCounter">32 people liked it</span>
+                        <span className="postLikeCounter">{post.like} People like this</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 Comments</span>
+                        <span className="postCommentText">{post.comment} Comments</span>
                     </div>
                 </div>
             </div>
---
+++ FrontEnd/src/dummyData.js
@@ -0,0 +1,213 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/person/rume.jpg",
+        username: "Magfur Rume"
+    },
+    {
+        id: 2,
+        profilePicture: "assets/person/ishmam.jpg",
+        username: "Ishmam Ul Hoque"
+    },
+    {
+        id: 3,
+        profilePicture: "assets/person/rayin.jpg",
+        username: "Syed Rayin"
+    },
+    {
+        id: 4,
+        profilePicture: "assets/person/tanzeem.jpg",
+        username: "Khandaker Shefat Noor Tanzeem"
+    },
+    {
+        id: 5,
+        profilePicture: "assets/person/nishat.jpg",
+        username: "Abdullah Arif Nishat"
+    },
+    {
+        id: 6,
+        profilePicture: "assets/person/ferdib.jpg",
+        username: "Ferdib Al Islam"
+    },
+    {
+        id: 7,
+        profilePicture: "assets/person/amita.jpg",
+        username: "Ferzana Amita"
+    },
+    {
+        id: 8,
+        profilePicture: "assets/person/rony.jpg",
+        username: "Mahfuz Rony"
+    },
+    {
+        id: 9,
+        profilePicture: "assets/person/mahima.jpg",
+        username: "Mahima Akter Mou"
+    },
+    {
+        id: 10,
+        profilePicture: "assets/person/sakib.jpg",
+        username: "Abid Sakib"
+    },
+    {
+        id: 11,
+        profilePicture: "assets/person/debu.jpg",
+        username: "Debashis Dutta"
+    }
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Something",
+        photo: "assets/post/1.jpg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,
+    },
+    {
+        id: 2,
+        desc: "Something",
+        photo: "assets/post/2.jpg",
+        date: "32 mins ago",
+        userId: 2,
+        like: 101,
+        comment: 62,
+    },
+    {
+        id: 3,
+        desc: "Something",
+        photo: "assets/post/3.jpg",
+        date: "58 mins ago",
+        userId: 1,
+        like: 51,
+        comment: 16,
+    },
+    {
+        id: 4,
+        desc: "Something",
+        photo: "assets/post/4.jpg",
+        date: "16 mins ago",
+        userId: 4,
+        like: 14,
+        comment: 2,
+    },
+    {
+        id: 5,
+        desc: "Something",
+        photo: "assets/post/5.jpg",
+        date: "26 mins ago",
+        userId: 6,
+        like: 67,
+        comment: 19,
+    },
+    {
+        id: 6,
+        desc: "Something",
+        photo: "assets/post/6.jpg",
+        date: "41 mins ago",
+        userId: 6,
+        like: 82,
+        comment: 9,
+    },
+    {
+        id: 7,
+        desc: "Something",
+        photo: "assets/post/7.jpg",
+        date: "36 mins ago",
+        userId: 3,
+        like: 6,
+        comment: 1,
+    },
+    {
+        id: 8,
+        desc: "Something",
+        photo: "assets/post/8.jpg",
+        date: "34 mins ago",
+        userId: 10,
+        like: 34,
+        comment: 11,
+    },
+    {
+        id: 9,
+        desc: "Something",
+        photo: "assets/post/9.jpg",
+        date: "53 mins ago",
+        userId: 11,
+        like: 62,
+        comment: 5,
+    },
+    {
+        id: 10,
+        desc: "Something",
+        photo: "assets/post/10.jpg",
+        date: "39 mins ago",
+        userId: 11,
+        like: 49,
+        comment: 5,
+    },
+    {
+        id: 11,
+        desc: "Something",
+        photo: "assets/post/11.jpg",
+        date: "30 mins ago",
+        userId: 9,
+        like: 62,
+        comment: 0,
+    },
+    {
+        id: 12,
+        desc: "Something",
+        photo: "assets/post/12.jpg",
+        date: "19 mins ago",
+        userId: 7,
+        like: 80,
+        comment: 32,
+    },
+    {
+        id: 13,
+        desc: "Something",
+        photo: "assets/post/13.jpg",
+        date: "59 mins ago",
+        userId: 7,
+        like: 250,
+        comment: 100,
+    },
+    {
+        id: 14,
+        desc: "Something",
+        photo: "assets/post/14.jpg",
+        date: "52 mins ago",
+        userId: 6,
+        like: 178,
+        comment: 69,
+    },
+    {
+        id: 15,
+        desc: "Something",
+        photo: "assets/post/15.jpg",
+        date: "9 mins ago",
+        userId: 6,
+        like: 17,
+        comment: 1,
+    },
+    {
+        id: 16,
+        desc: "Something",
+        photo: "assets/post/16.jpg",
+        date: "12 mins ago",
+        userId: 2,
+        like: 26,
+        comment: 2,
+    },
+    {
+        id: 17,
+        desc: "Something",
+        photo: "assets/post/17.jpg",
+        date: "19 mins ago",
+        userId: 5,
+        like: 41,
+        comment: 9,
+    }
+]
