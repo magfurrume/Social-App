8c8faedc4513ea8525993303ceb7cdca54ef8336
Md. Magfur Alam <magfurrume@gmail.com>
2023-02-04 23:53:28 +0600

    vercel-backend

--- FrontEnd/src/componenets/search/Search.jsx
+++ FrontEnd/src/componenets/search/Search.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import "./search.css";
+import { Link } from 'react-router-dom';
 
 function Search() {
     const API_CALL = process.env.REACT_APP_BACKEND_API;
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
     const handleSearch = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.get(`http://localhost:8800/api/users/search?name=${searchTerm}`);
+            const res = await axios.get(API_CALL+`users/search?name=${searchTerm}`);
             setSearchResults(res.data);
         } catch (err) {
             console.error(err);
@@ -17,7 +20,7 @@ function Search() {
     };
 
     return (
-        <div>
+        <div className='mainDiv'>
             <form onChange={handleSearch}>
                 <p>Search</p>
                 <input
@@ -26,13 +29,26 @@ function Search() {
                     onChange={e => setSearchTerm(e.target.value)}
                     placeholder="Search for items..."
                 />
-                <button type="submit">Search</button>
             </form>
             <ul>
-                {searchResults.map(item => (
-                    <li key={item._id}>{item.userName}</li>
+                {searchResults.map(people => (
+                    <div key={people._id} style={{ cursor: "pointer" }}>
+                        <Link
+                            to={"/profile/" + people.userName}
+                            style={{ color: 'inherit', textDecoration: 'inherit' }}
+                        >
+                            <div className='searchItem' >
+                                <img src={people.profilePicture ? PF + people.profilePicture : PF + 'person/noAvater.png'} className="searchImg" />
+                                {people.userName}
+                            </div>
+                        </Link>
+
+                    </div>
                 ))}
             </ul>
+            <div>
+                rume
+            </div>
         </div>
     );
 }
--- FrontEnd/src/componenets/topbar/Topbar.jsx
+++ FrontEnd/src/componenets/topbar/Topbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./topbar.css";
 import { Search, Chat, Notifications, Person, ExpandMore } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Topbar() {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
     ////
@@ -16,8 +19,19 @@ export default function Topbar() {
         navigate("/login")
     };
 
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.get(`http://localhost:8800/api/users/search?name=${searchTerm}`);
+            setSearchResults(res.data);
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
 
+    console.log(searchTerm);
     return (
         <div className='topbarContainer'>
             <div className="topbarLeft">
@@ -25,11 +39,38 @@ export default function Topbar() {
                     <span className='logo'>facebook</span>
                 </Link>
             </div>
-            <div className="topbarCenter">
-                <div className="searchbar">
+            <div className="topbarCenter" onChange={handleSearch}>
+                <form className="searchbar">
                     <Search className='searchIcon' />
-                    <input placeholder='Search Facebook' className="searchInput" />
-                </div>
+                    <input
+                        placeholder='Search Facebook'
+                        className="searchInput"
+                        type="text"
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </form>
+                {searchResults.length > 0 && (
+                    <div className='searchResultContainer'>
+                        <div style={{ width: '100%' }}>
+                            {searchResults.map(people => (
+                                <div key={people._id} style={{ cursor: "pointer" }}>
+                                    <Link
+                                        to={"/profile/" + people.userName}
+                                        style={{ color: 'inherit', textDecoration: 'inherit' }}
+                                    >
+                                        <div className='searchItem' >
+                                            <img src={people.profilePicture ? PF + people.profilePicture : PF + 'person/noAvater.png'} className="searchImg" />
+                                            {people.userName}
+                                        </div>
+                                    </Link>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+
             </div>
             <div className="topbarRight">
                 <div className="topbarRightContent">
